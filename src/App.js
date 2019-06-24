57e37bd28fd3cba18d3e9c874de83369ab0bceae
elizhang227 <zhang.eli@hotmail.com>
2019-06-24 13:49:20 -0400

    created card and styled it with image and stripes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import MemoryCard from './MemoryCard.js';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+        <header className="App-header">
+            <h1>
+            Memory Game
+            </h1>
+            <p>Match cards to win</p>
+        </header>
+        <MemoryCard>CARD</MemoryCard>
+        </div>
+    );
 }
 
 export default App;
