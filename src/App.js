69fb2821eb7a6de8fa3b59c0bf98f6fa96b37996
elizhang227 <zhang.eli@hotmail.com>
2019-06-25 20:49:18 -0400

    clicking on card now flips the card

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MemoryCard from './MemoryCard.js';
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-        <header className="App-header">
-            <h1>
-            Memory Game
-            </h1>
-            <p>Match cards to win</p>
-        </header>
-        <div>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        </div>
-        <div>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        </div>
-        <div>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        </div>
-        <div>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        </div>
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+            <header className="App-header">
+                <h1>
+                Memory Game
+                </h1>
+                <p>Match cards to win</p>
+            </header>
+            <div>
+            <MemoryCard></MemoryCard>
+            <MemoryCard></MemoryCard>
+            <MemoryCard></MemoryCard>
+            <MemoryCard></MemoryCard>
+            </div>
+            <div>
+            <MemoryCard></MemoryCard>
+            <MemoryCard></MemoryCard>
+            <MemoryCard></MemoryCard>
+            <MemoryCard></MemoryCard>
+            </div>
+            <div>
+            <MemoryCard></MemoryCard>
+            <MemoryCard></MemoryCard>
+            <MemoryCard></MemoryCard>
+            <MemoryCard></MemoryCard>
+            </div>
+            <div>
+            <MemoryCard></MemoryCard>
+            <MemoryCard></MemoryCard>
+            <MemoryCard></MemoryCard>
+            <MemoryCard></MemoryCard>
+            </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/MemoryCard.js
+++ src/MemoryCard.js
@@ -2,10 +2,34 @@ import React, { Component } from 'react';
 import './MemoryCard.css';
 
 class MemoryCard extends Component {
+    constructor() {
+        super();
+        this.state = {isFlipped: false};
+    }
+    // state = {
+    //     isFlipped: false
+    // }
+
+    clickHandler = () => {
+        this.setState({
+            isFlipped: !this.state.isFlipped
+        })
+    }
+
     render() {
+        let memoryCardInnerClass = 'MemoryCardInner';
+        if (!!this.state.isFlipped) {
+            memoryCardInnerClass += ' flipped';
+        }
+        console.log(memoryCardInnerClass);
         return (
-        <div className='MemoryCard'>
-            <img src='https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png' alt="dc-log"></img>
+        <div className='MemoryCard' onClick={this.clickHandler.bind(this)}>
+            <div className={memoryCardInnerClass}>
+                <div className='MemoryCardBack'>
+                    <img src='https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png' alt="dc-log"></img>
+                </div>
+                <div className='MemoryCardFront'>∆</div>
+            </div>
         </div>
         );
     }
